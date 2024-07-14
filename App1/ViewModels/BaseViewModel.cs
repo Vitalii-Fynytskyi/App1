cd32cbf4a5de60f4c0f31beecc9dcdd33705785c
Vitalii-Fynytskyi <fynytsky01@gmail.com>
2024-07-14 12:17:38 +0300

    Add project files.

---
+++ App1/ViewModels/BaseViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace App1.ViewModels
+{
+    [Flags]
+    public enum ViewModelStatus : byte
+    {
+        NotChanged = 0,
+        Updated = 1,
+        Inserted = 2,
+        Deleted = 4
+    }
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public ViewModelStatus ViewModelStatus { get; set; } = ViewModelStatus.NotChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public virtual void NotifyPropertyChanged(params string[] propertyNames)
+        {
+            if (PropertyChanged != null)
+            {
+                foreach (string propertyName in propertyNames)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+        }
+        public virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
